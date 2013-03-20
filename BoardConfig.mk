0cb935e43e4271544f659299ec5462815d1f0a36
garwedgess <garethwilliams21@gmail.com>
2013-03-20 00:21:35 +0000

    BoardConfig:Added bluetooth path for 4.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,18 @@
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
 -include device/semc/zeus-common/BoardConfigCommon.mk
--include vendor/semc/zeusc/BoardConfigVendor.mk
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/semc/zeusc/bluetooth
+
+# kernel
 TARGET_KERNEL_SOURCE := kernel/semc/msm7x30
 TARGET_KERNEL_CONFIG := gar_zeus_defconfig
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 
 TARGET_RECOVERY_INITRC := device/semc/zeusc/recovery/init.rc
 
+# sensor
 SENSORS_COMPASS_AK8975 := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
@@ -19,3 +25,4 @@ BOARD_USES_STEREO_HW_SPEAKER := true
 
 TARGET_OTA_ASSERT_DEVICE := R800x,zeusc
 
+-include vendor/semc/zeusc/BoardConfigVendor.mk
